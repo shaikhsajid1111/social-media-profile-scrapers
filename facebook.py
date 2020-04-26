f4b74db188ac3e8e3ccd8bda6c0bacbcd9c2be40
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-04-26 17:35:35 +0530

    added for facebook

---
+++ facebook.py
@@ -0,0 +1,39 @@
+import requests 
+from bs4 import BeautifulSoup
+import requests
+
+class Facebook():
+    def __init__(self,username):
+        self.username = username
+    def scrap(self):
+        try:
+            URL = f"https://facebook.com/{self.username}"
+            respond = requests.get(URL)
+            if respond.status_code == 404:
+                print("Could Not Connect or User does not exist!\n")
+            if respond.status_code == 200:
+                soup = BeautifulSoup(respond.content,"html.parser")
+                facebook_name = soup.find("span",{
+                    "id":"fb-timeline-cover-name"
+                })
+                profile_image = soup.find("img",{
+                    "class" : "_11kf img"
+                })
+                current_city = soup.find("li",{
+                    "id" : "current_city"
+                })
+                clg = soup.find("div",{
+                    "class" : "_2lzr _50f5 _50f7"
+                })
+                #print(profile_image['src'])
+                #print(facebook_name.text.strip())
+                return {
+                    "profile_image" : profile_image['src'],
+                    "current_city" : current_city.text.strip(),
+                    "Education" : clg.text.strip()
+                }
+        except Exception as ex:
+             print(ex)       
+
+fb = Facebook("shaikhsajid1111")
+print(fb.scrap())        
