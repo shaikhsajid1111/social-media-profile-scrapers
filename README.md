41ecb61539d5e1a97f0a7b85c22db9fecd3ff5f8
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-05-16 11:35:21 +0530

    added quora script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Social Media Crawlers
+# Social Media Profile Crawlers
 
 ## Available Social Media
 - Twitter :+1:
@@ -7,21 +7,31 @@
 - Reddit :+1:   
 - tiktok :+1:
 - Medium :+1:
+- Quora :+1:
+
+
 ## To do
 
 - Pinterest :clock1:
-- Quora :clock1:
+
 ## How to run these scripts?
 1. You need person's social media username to fetch detail
+1. Install dependencies mentioned inside [requirement.txt](requirement.txt) by entering command ```pip install -r requirement.txt``` from project's directory directory
 1. There are 2 ways to input username
     - Edit source code of the script and pass username as a constructor's parameter
     - Pass username from command line
-        - Open terminal and type command ```python SCRIPT_NAME USERNAME```
-1. To run Tiktok's scripts, install chromedriver from [here]('https://chromedriver.chromium.org/downloads'). Chrome browser's version must be same as chromedriver's version
-**More social media will be added :sleepy:**
+        - Open terminal in project's directory and type command ```python SCRIPT_NAME USERNAME```
+1. Some scripts use chromdriver, install chromedriver from [here](https://chromedriver.chromium.org/downloads). Chrome browser's version must be same as chromedriver's version
+
 - If server stop responding because of too much request, so try modifying requests.get() with below given code:
     - ```requests.get(URL,headers = headers,proxies={"http": "http://111.233.225.166:1234"})```
                                 or
     - ```requests.get(URL,headers = headers,verify = False)```
                                 or
-    - ```requests.get(URL,headers = headers,timeout = 5)```                
+    - ```requests.get(URL,headers = headers,timeout = 5)```
+                            
+
+
+
+
+**More social media will be added :sleepy:**
--- quora.py
+++ quora.py
@@ -1,4 +1,3 @@
-import requests
 import sys
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -7,12 +6,18 @@ import time
 class Quora:
     def __init__(self,username = sys.argv[len(sys.argv)-1]):
         self.username = username
-
+    def is_none(self,val):
+        if val is None:
+            val = 'Not Found!'
+            return val
+        else:
+            return val
+            
     def scrap(self):
         try:
             URL = f'https://quora.com/profile/{self.username}'
             print(URL)
-            #headers = {'user-agent' : 'Your User Agent'}
+          
             chrome_option = Options()
             chrome_option.add_argument('--headless')
             chrome_option.add_argument('--disable-extensions')
@@ -21,31 +26,62 @@ class Quora:
             driver.get(URL)
             time.sleep(5)
             response = driver.page_source.encode('utf-8').strip()
+             
             soup = BeautifulSoup(response,'html.parser')
+       
             name = soup.find('div',{
                 'class' : "q-text qu-bold"
             })
+            name = self.is_none(name)
+            
             profession = soup.find('div',{
                 'class' : 'q-text qu-fontSize--large'
             })
+            profession = self.is_none(profession)
+            
             profile_image = soup.find('img',{
                 'class' : 'q-image qu-display--block'
             })
+            profile_image = self.is_none(profile_image)
+
             bio = soup.find('p',{
                 'class' : 'q-text qu-display--block'
             })
-            print(profession.text)
-            print(name.text)
-            print(profile_image['src'])
-            print(bio)
-            """ if response.status_code == 404:
-                print("Failed to connect or user does not exist!")
-                exit()
-            if response.status_code == 200:   
-                soup = BeautifulSoup(response.content,'html.parser')
-                print(soup.prettify()) """
-                
+            bio = self.is_none(bio)
+
+            answers_count = soup.find('div',{
+                'class' : 'q-text qu-medium qu-fontSize--small qu-color--red'
+            })
+
+            detail_count = soup.find_all('div',{
+                'class' : 'q-text qu-medium qu-fontSize--small qu-color--gray_light'
+            })
+            if detail_count is not None:
+                questions = detail_count[0]
+                shares = detail_count[1]
+                posts = detail_count[2]
+                followers = detail_count[3]
+                followings = detail_count[4]
+            else:
+                questions,shares,posts,followers,followings = ''    
+            more = soup.find_all('div',{
+                'class' : 'q-text qu-truncateLines--2'
+            })
+            
+            return {
+                'name'  :name.text,
+                'profession' : profession.text.strip(),
+                'profile_image' : profile_image['src'] if type(profile_image) is not str else profile_image,
+                'bio' : bio if type(bio) is str else bio.text,
+                'answers_count' : answers_count.text.strip(),
+                'questions_count' : questions.text.strip(),
+                'shares' : shares.text.strip(),
+                'posts' : posts.text.strip(),
+                'followers' : followers.text.strip(),
+                'following' : followings.text.strip(),
+                'more_details' : [more[i].text.replace('\n','').replace('\r','') for i in range(len(more))]
+            }
         except Exception as ex:
             print(ex)        
-usr = Quora('Kashish-Arora-60')
+usr = Quora()
 print(usr.scrap())                
