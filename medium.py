f08b122baf9e756ebcf30fdcf1f7e1608e61932f
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-06-16 18:06:56 +0530

    updated selectors

--- medium.py
+++ medium.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import sys
+from fake_headers import Headers
 
 class Medium:
     def __init__(self,username = sys.argv[len(sys.argv)-1]):
@@ -9,27 +10,28 @@ class Medium:
         try:
             url = f"https://medium.com/@{self.username}"
             
-            headers = {'user-agent' : 'Your User Agent'}
+            headers = Headers().generate()
             
-            respond = requests.get(url)
+            respond = requests.get(url,headers = headers)
             if respond.status_code == 404:
                 print("Failed to connect or user does not exist!")
                 exit()
             if respond.status_code == 200:    
                 soup = BeautifulSoup(respond.content,"html.parser")
-    
+                
                 #user have paid membership
                 full_name = soup.find('h1',{
-                    'class' :  'av q dp cj dq ck dr ds dt y'
+                    'class' :  'av q do ci dp cj dq dr ds y'
                      
                 })
                 is_paid_member = True
                 bio = soup.find('p',{
-                    'class' : 'eq er cj b ck es et cn y'
+                    'class' : 'ep eq ci b cj er es cm y'
                 })
                 subtitle = soup.find_all('a',{
-                    'class' :'cd ce bm bn bo bp bq br bs bt ex bw bx ch ci'
+                    'class' :'cc cd bm bn bo bp bq br bs bt ew bw cg ch'
                 })
+                
                 extra_info = [subtitle[i].text for i in range(5) if len(subtitle) > 0]
                 
                 if full_name and bio and subtitle is not None:
@@ -45,14 +47,14 @@ class Medium:
         #if free account
         else:
             full_name = soup.find('h1',{
-                'class' : 'av q dh cj di ck dj dk dl y'
+                'class' : 'av q dg ci dh cj di dj dk y'
             })      
             is_paid_member = False
             bio = soup.find('p',{
-                'class' : 'ei ej cj b ck ek el cn y'
+                'class' : 'eh ei ci b cj ej ek cm y'
             })
             subtitle = soup.find_all('a',{
-                'class' : 'cd ce bm bn bo bp bq br bs bt eo bw bx ch ci'
+                'class' : 'cc cd bm bn bo bp bq br bs bt eo bw cg ch'
             })
             extra_info = [subtitle[i].text for i in range(5) if len(subtitle) > 0]
             if full_name and bio is not None:              
@@ -64,3 +66,6 @@ class Medium:
                     }
 med = Medium()
 print(med.scrap())            
+'''
+last modified on : 16th June,2020
+'''
