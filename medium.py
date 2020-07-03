f52727d743431d1fbe11d2614a5062cef87a64e1
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-07-03 12:28:43 +0530

    updated script according to  medium's source code, added feature to fetch profile image links as well, code refactorization from request module to selenium

--- medium.py
+++ medium.py
@@ -1,74 +1,101 @@
-import requests
-from bs4 import BeautifulSoup
 import sys
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from fake_headers import Headers
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import re
 class Medium:
+    @staticmethod   
+    def init_driver(driver_path:str,browser_name:str):
+        '''initiailize driver'''
+        def set_properties(browser_option):
+            '''set properties for the driver'''
+            ua = Headers().generate()      #fake user agent
+            browser_option.add_argument('--headless')
+            browser_option.add_argument('--disable-extensions')
+            browser_option.add_argument('--incognito')
+            browser_option.add_argument('--disable-gpu')
+            browser_option.add_argument('--log-level=3')
+            browser_option.add_argument(f'user-agent={ua}')
+            return browser_option
+        try:
+            browser_name = browser_name.strip().title()
+
+            
+            #automating and opening URL in headless browser
+            if browser_name == "Chrome":
+                browser_option = ChromeOptions()
+                browser_option = set_properties(browser_option)    
+                driver = webdriver.Chrome(driver_path,options=browser_option) #chromedriver's path in first argument
+            elif browser_name == "Firefox":
+                browser_option = FirefoxOptions()
+                browser_option = set_properties(browser_option)
+                driver = webdriver.Firefox(driver_path,options=browser_option)
+            else:
+                driver = "Browser Not Supported!"
+            return driver
+        except Exception as ex:
+            print(ex)
+    
     @staticmethod    
     def scrap(username):
+        """scrap medium's profile"""
         try:
-            url = f"https://medium.com/@{username}"
+            URL = f"https://medium.com/@{username}"
+           
+            driver = Medium.init_driver('C:\\webdrivers\\chromedriver.exe',"Chrome") #replace with your webdriver's path and browser's name
+            driver.get(URL)
+            #wait until page loads so title contains "Medium"
+            wait = WebDriverWait(driver, 10)
+            element = wait.until(EC.title_contains('Medium'))
+            #get source code of the page
+            response = driver.page_source.encode('utf-8').strip()
+             
+            soup = BeautifulSoup(response,'html.parser')  
             
-            headers = Headers().generate()
+            profile_image = soup.find("meta" , {"property" : "og:image"})["content"]   #profile image from meta tag
+            profile_username = soup.find("meta" , {"property" : "profile:username"})['content'] #profile username so, it can be used to create URL in following,follwers vars
+            #if this finds full name then the account is premium
+            full_name = soup.find('h1',{
+                'class' :  'aw q do ci dp cj dq dr ds z'
+                 
+            })
             
-            respond = requests.get(url,headers = headers)
-            if respond.status_code == 404:
-                print("Failed to connect or user does not exist!")
-                exit()
-            if respond.status_code == 200:    
-                soup = BeautifulSoup(respond.content,"html.parser")
-                
-                #user have paid membership
+            bio = soup.find('p',{
+                'class' : 'ep eq ci b cj er es cm z'
+            })
+            is_paid_member = True
+            #if full name was not fetched then account is free
+            if full_name is None:
                 full_name = soup.find('h1',{
-                    'class' :  'av q do ci dp cj dq dr ds y'
-                     
-                })
-                is_paid_member = True
+                'class' : 'aw q dg ci dh cj di dj dk z'
+            })      
                 bio = soup.find('p',{
-                    'class' : 'ep eq ci b cj er es cm y'
-                })
-                subtitle = soup.find_all('a',{
-                    'class' :'cc cd bm bn bo bp bq br bs bt ew bw cg ch'
-                })
-                
-                extra_info = [subtitle[i].text for i in range(5) if len(subtitle) > 0]
-                
-                if full_name and bio and subtitle is not None:
-                    return {
-                        'full_name' : full_name.text,
+                'class' : 'eh ei ci b cj ej ek cm z'
+            })
+                is_paid_member = False  
+                 
+            followings = soup.find('a',{'href' : f"/@{profile_username}/following"}) 
+            followers = soup.find('a',{'href' : f"/@{profile_username}/followers"})
+            return {
+                        "profile_image" : profile_image, 
+                        'full_name' : full_name.text if full_name is not None else "No name found",
                         'is_paid_member' : is_paid_member,
-                        'bio' : bio.text,
-                        'extras' : extra_info,
-                       
+                        'bio' : bio.text if bio is not None else "Bio Not Found!",
+                        "followings" : followings.text if followings is not None else "Not found!",
+                        "followers" : followers if followers is not None else "Not Found!"
                     }    
         except Exception as ex:
             print(ex)
-        #if free account
-        else:
-            full_name = soup.find('h1',{
-                'class' : 'av q dg ci dh cj di dj dk y'
-            })      
-            is_paid_member = False
-            bio = soup.find('p',{
-                'class' : 'eh ei ci b cj ej ek cm y'
-            })
-            subtitle = soup.find_all('a',{
-                'class' : 'cc cd bm bn bo bp bq br bs bt eo bw cg ch'
-            })
-            extra_info = [subtitle[i].text for i in range(5) if len(subtitle) > 0]
-            if full_name and bio is not None:              
-                return {
-                        'full_name' : full_name.text,
-                        'is_paid_member' : is_paid_member,
-                        'bio' : bio.text,
-                        'extras' : extra_info
-                    }
-
-
-
+        
 
 if __name__ == '__main__':
     print(Medium.scrap(sys.argv[ len(sys.argv)-1] ))           
+
 '''
-last modified on : 16th June,2020
+last modified on : 3rd July,2020
 '''
