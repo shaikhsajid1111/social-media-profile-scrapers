b89d1ae54eebb791fffb72ebe2ec5f6a03e6f0af
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-08-20 11:01:06 +0530

    github script: removed bs4 dependency

--- github.py
+++ github.py
@@ -1,6 +1,5 @@
 try:
     import argparse
-    from bs4 import BeautifulSoup
     from selenium import webdriver
     from selenium.webdriver.chrome.options import Options as ChromeOptions
     from selenium.webdriver.firefox.options import Options as FirefoxOptions
@@ -9,6 +8,7 @@ try:
     from selenium.webdriver.support.ui import WebDriverWait
     from selenium.webdriver.support import expected_conditions as EC
     from settings import DRIVER_SETTINGS
+    from selenium.common.exceptions import NoSuchElementException
 except ModuleNotFoundError:
     print("Please download dependencies from requirement.txt")
 except Exception as ex:
@@ -63,42 +63,34 @@ class Github:
            
             
             driver.get(URL)            
+            
             #wait until page loads
             wait = WebDriverWait(driver, 10)
             element = wait.until(EC.title_contains(f"{username}"))
-            #get source code of the website
-            response = driver.page_source.encode('utf-8').strip()
             
-            soup =  BeautifulSoup(response,'html.parser')
-            #finding all elements in source code
-            full_name = soup.find("span",{
-                'class' : 'p-name vcard-fullname d-block overflow-hidden'
-            })
-            bio = soup.find('div',{
-                'class' : 'p-note user-profile-bio mb-3 js-user-profile-bio f4'
-            })
+            full_name = driver.find_element_by_css_selector("span.p-name.vcard-fullname.d-block.overflow-hidden")
+            try:
+                bio = driver.find_element_by_css_selector("div.p-note.user-profile-bio.mb-3.js-user-profile-bio.f4")
+            except NoSuchElementException:
+                bio = ""
+            try:
+                location = driver.find_element_by_css_selector("span.p-label")
+            except NoSuchElementException:
+                location = ""
+            try:
+                email = driver.find_element_by_css_selector("li[itemprop='email']")
+            except NoSuchElementException:
+                email = ""
             
-            location = soup.find('span',{
-                'class' : 'p-label'
-            })
-            status = soup.find('div',{
-                'class' : 'ws-normal user-status-message-wrapper f6 min-width-0'
-            })
-            
-            email = soup.find('a',{
-                'itemprop' : 'u-email link-gray-dark '
-            })
-            contributions = soup.find_all("h2",{
-                "class" : 'f4 text-normal mb-2'
-            })[0]
-            driver.close()
-            driver.quit()
+            try:
+                contributions = driver.find_element_by_css_selector(".js-yearly-contributions")
+            except NoSuchElementException:
+                contributions = "" 
             return {
-                    'full_name' : full_name.text if full_name is not None else "Not Found" ,
-                    'bio' : bio.text if bio is not None else "Bio Not Found!",
-                    'location' : location.text if location is not None else "Location Not found!",
-                    'status':  status.text if status is not None else "No status given",
-                    "contributions" : contributions.text.strip().replace("\n","")
+                    'full_name' : full_name.text,
+                    'bio' : bio.text if type(bio) is not str else "",
+                    'location' : location.text if type(location) is not str else "",
+                    "contributions" : contributions.text.split(" ")[0] if type(contributions) is not str else ""
                                    }
         except Exception as ex:
             driver.close()
@@ -112,4 +104,4 @@ if __name__ == '__main__':
     print(Github.scrap(args.username))
 
 
-#last updated on 31st July, 2020
+#last updated on 20th August, 2020
