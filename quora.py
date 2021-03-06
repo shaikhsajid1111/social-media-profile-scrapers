018bce5f847a603f23d6ee0e35fdf1320222734a
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2021-03-06 11:00:48 +0530

    fupdated selectors

--- quora.py
+++ quora.py
@@ -62,6 +62,7 @@ class Quora:
             element = wait.until(EC.title_contains('Quora'))
 
             name = driver.find_element_by_css_selector("div.q-text.qu-bold")
+            
             try:
                 profession = driver.find_element_by_css_selector("div.q-text.qu-fontSize--large")
             except:
@@ -76,47 +77,45 @@ class Quora:
             except:
                 bio = ""
             try:
-                driver.find_element_by_class_name("q-absolute").click()
-            except:
-                pass
+                driver.find_element_by_css_selector("div.qt_read_more").click()
+            except Exception as ex:
+                print(ex)
+            
             try:
-                answers_count = driver.find_element_by_css_selector('div.q-text.qu-medium.qu-fontSize--small.qu-color--red')
-            except:
-                answers_count = ""
-            try:
-                detail_count = driver.find_elements_by_css_selector('div.q-text.qu-medium.qu-fontSize--small.qu-color--gray_light')
-                questions = detail_count[0]
-                shares = detail_count[1]
-                posts = detail_count[2]
-                followers = detail_count[3]
-                
-                #followings = detail_count[4]
+                details = driver.find_elements_by_css_selector('div.q-box.qu-overflowX--hidden.qu-whiteSpace--nowrap')
+                details_text = details[0].text.split('\n')
+            
+                answers_count = details_text[1].split(' ')[0]
+                questions = details_text[2].split(' ')[0]
+                shares = details_text[3].split(' ')[0]
+                posts = details_text[4].split(' ')[0]
+                followers = details_text[5].split(' ')[0]
                           
-            except:
-                questions,shares,posts,followers,followings = ''    
+            except Exception as ex:
+                print(ex)
+                questions = shares = posts = followers = answers_count = ''    
+            
+            
             try:
                 more_button = driver.find_element_by_name("ChevronDown").click()
                 popup = driver.find_element_by_class_name("qu-zIndex--popover")
                 all_divs = popup.find_elements_by_css_selector("div")
-                followings = all_divs[0]
-            except:
+                followings = all_divs[0].text.split(" ")[0]
+            except Exception as ex:
+                print(ex)
                 followings = ""
-            try:
-                more = driver.find_elements_by_css_selector("div.q-text.qu-truncateLines--2")
-            except:
-                more = ""
+            
             profile_data = {
                 'name'  :name.text,
                 'profession' : profession.text.strip(),
                 'profile_image' : profile_image.get_attribute("src"),
                 'bio' : bio.text,
-                'answers_count' : answers_count.text.strip().split(" ")[0],
-                'questions_count' : questions.text.strip().split(" ")[0],
-                'shares' : shares.text.strip().split(" ")[0],
-                'posts' : posts.text.strip().split(" ")[0],
-                'followers' : followers.text.strip().split(" ")[0],
-                'following' : followings.text.split(" ")[0],
-                'more_details' : [more[i].text.replace('\n','').replace('\r','') for i in range(len(more))] if type(more) is not str else ""
+                'answers_count' : answers_count,
+                'questions_count' : questions,
+                'shares' : shares,
+                'posts' : posts,
+                'followers' : followers,
+                'following' : followings,
             }
             driver.close()
             driver.quit()
@@ -135,4 +134,4 @@ if __name__ == '__main__':
     browser_name = args.browser if args.browser is not None else "chrome"
     print(Quora.scrap(args.username,browser_name))
 
-#last updated - 11th September, 2020
+#last updated - 6th March,2021
