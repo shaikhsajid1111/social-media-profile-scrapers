6a6c7ce477a091e8cbec1807fe04bbcbf6ba018f
Sajid Shaikh <shaikhsajid11112000@gmail.com>
2023-01-01 10:25:26 +0530

    Updated script to use API instead of browser automation

--- instagram.py
+++ instagram.py
@@ -1,16 +1,8 @@
 try:
     import argparse
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options as ChromeOptions
-    from selenium.webdriver.firefox.options import Options as FirefoxOptions
-    from selenium.webdriver.support import expected_conditions as EC
-    from selenium.webdriver.support.ui import WebDriverWait
-    from fake_headers import Headers
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.firefox import GeckoDriverManager
-    from selenium.common.exceptions import NoSuchElementException
     import json
-    from selenium.webdriver.common.by import By
+    import requests
+    from fake_headers import Headers
 except ModuleNotFoundError:
     print("Please download dependencies from requirement.txt")
 except Exception as ex:
@@ -22,180 +14,69 @@ except Exception as ex:
 
 class Instagram:
     @staticmethod
-    def init_driver(browser_name):
-        '''init the driver'''
-        def set_properties(browser_option):
-            '''sets the driver's properties'''
-            ua = Headers().generate()  # fake user agent
-            browser_option.add_argument('--headless')
-            browser_option.add_argument('--disable-extensions')
-            browser_option.add_argument('--incognito')
-            browser_option.add_argument('--disable-gpu')
-            browser_option.add_argument('--log-level=3')
-            browser_option.add_argument(f'user-agent={ua}')
-            browser_option.add_argument('--disable-notifications')
-            browser_option.add_argument('--disable-popup-blocking')
-            return browser_option
-        try:
-            browser_name = browser_name.strip().title()
-
-            # automating and opening URL in headless browser
-            if browser_name.lower() == "chrome":
-                browser_option = ChromeOptions()
-                browser_option = set_properties(browser_option)
-                driver = webdriver.Chrome(
-                    ChromeDriverManager().install(), options=browser_option)
-            elif browser_name.lower() == "firefox":
-                browser_option = FirefoxOptions()
-                browser_option = set_properties(browser_option)
-                driver = webdriver.Firefox(
-                    executable_path=GeckoDriverManager().install(), options=browser_option)
-            else:
-                driver = "Browser Not Supported!"
-            return driver
-        except Exception as ex:
-            print(ex)
+    def build_param(username):
+        params = {
+            'username': username,
+        }
+        return params
 
     @staticmethod
-    def find_bio(driver):
-        try:
-            element = driver.find_element_by_css_selector('div._aa_c')
-            return element.text
-        except NoSuchElementException:
-            return ''
-        except Exception as ex:
-            print("Error at find_bio : {}".format(ex))
+    def build_headers(username):
+        return {
+            'authority': 'www.instagram.com',
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.9',
+            'referer': f'https://www.instagram.com/{username}/',
+            'sec-ch-prefers-color-scheme': 'dark',
+            'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Microsoft Edge";v="108"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': Headers().generate()['User-Agent'],
+            'x-asbd-id': '198387',
+            'x-csrftoken': 'VUm8uVUz0h2Y2CO1SwGgVAG3jQixNBmg',
+            'x-ig-app-id': '936619743392459',
+            'x-ig-www-claim': '0',
+            'x-requested-with': 'XMLHttpRequest',
+        }
 
     @staticmethod
-    def value_to_float(x):
-        try:
-            x = float(x)
-            return x
-        except:
-            pass
-        x = x.lower()
-        if 'k' in x:
-            if len(x) > 1:
-                return float(x.replace('k', '')) * 1000
-            return 1000
-        if 'm' in x:
-            if len(x) > 1:
-                return float(x.replace('m', '')) * 1000000
-            return 1000000
-        if 'm' in x:
-            return float(x.replace('m', '')) * 1000000000
-        return 0
-
-    @staticmethod
-    def find_profile_image_link(driver):
-        try:
-            profile_image = driver.find_element_by_css_selector('img[alt]')
-            return profile_image.get_attribute('src')
-        except NoSuchElementException:
-            print('Element Not Found')
-            profile_image = ''
-        # except Exception as ex:
-        #  print("Error at find_profile_image_link : {}".format(ex))
-
-    @staticmethod
-    def extract_posts_count(text):
-        try:
-            if 'Posts' in text:
-                text = text.split(" ")[-2]
-                text = text.replace(',', '')
-                return int(Instagram.value_to_float(text))
-        except Exception as ex:
-            print("Error at extract_posts_count: {}".format(ex))
-            return None
-
-    @staticmethod
-    def extract_followers_count(text):
-        try:
-            if 'Followers' in text:
-                return int(Instagram.value_to_float(text.split(" ")[0]))
-        except Exception as ex:
-            print("Error at extract_followers_count : {}".format(ex))
-            return None
-
-    @staticmethod
-    def extract_following_count(text):
-        try:
-            if 'Followers' in text:
-                return int(Instagram.value_to_float(text.split(" ")[2]))
-        except Exception as ex:
-            print("Error at extract_followers_count : {}".format(ex))
-            return None
-
-    @staticmethod
-    def find_bio(driver):
-        try:
-            elements = driver.find_element_by_css_selector('._aa_c  > h1')
-            return elements.get_attribute('textContent')
-        except NoSuchElementException:
-            print('Element Not Found')
-            return ''
-        except Exception as ex:
-            print("Error at find_bio : {}".format(ex))
-
-    @staticmethod
-    def is_private(driver):
-        try:
-            driver.find_element_by_xpath(
-                "//h2[text()='This Account is Private']")
-            return True
-        except NoSuchElementException:
-            return False
-        except Exception as ex:
-            print("Error at is_private : {} ".format(ex))
-
-    @staticmethod
-    def scrap(username, browser_name):
-        try:
-            URL = 'https://instagram.com/{}'.format(username)
-            try:
-                driver = Instagram.init_driver(browser_name)
-                driver.get(URL)
-            except AttributeError:
-                print("Driver is not set")
-                exit()
-
-            wait = WebDriverWait(driver, 30)
-            wait.until(EC.title_contains('@'))
-            wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'img[alt]')))
-            description_meta = driver.find_element_by_css_selector(
-                'meta[name="description"]')
-            meta_content = description_meta.get_attribute(
-                'content').split('-')[0]
-            posts_count = Instagram.extract_posts_count(meta_content.strip())
-            followers_count = Instagram.extract_followers_count(meta_content)
-            followings_count = Instagram.extract_following_count(meta_content)
-            profile_image = Instagram.find_profile_image_link(driver)
-            bio = Instagram.find_bio(driver)
-            is_private = Instagram.is_private(driver)
-            profile_data = {
-                'profile_image': profile_image,
-                'bio': bio,
-                "posts_count": posts_count,
-                "followers": followers_count,
-                "followings": followings_count,
-                "is_private": is_private
+    def make_request(url, params, headers, proxy=None):
+        response = None
+        if proxy:
+            proxy_dict = {
+                'http': f'http://{proxy}',
+                'https': f'http://{proxy}'
             }
-            driver.close()
-            driver.quit()
-            return json.dumps(profile_data)
+            response = requests.get(
+                url, headers=headers, params=params, proxies=proxy_dict)
+        else:
+            response = requests.get(
+                url, headers=headers, params=params)
+        return response
+
+    @staticmethod
+    def scrap(username, proxy = None):
+        try:
+            headers = Instagram.build_headers(username)
+            params = Instagram.build_param(username)
+            response = Instagram.make_request('https://www.instagram.com/api/v1/users/web_profile_info/',
+            headers=headers, params=params, proxy=proxy)
+            if response.status_code == 200:
+                profile_data = response.json()['data']['user']
+                return json.dumps(profile_data)
+            else:
+                print('Error : ', response.status_code, response.text)
         except Exception as ex:
-            driver.close()
-            driver.quit()
             print(ex)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("username", help="username to search")
-    parser.add_argument("--browser", help="Chrome or Firefox?")
+    parser.add_argument("--proxy", help="Proxy to use", default=None)
     args = parser.parse_args()
-    browser_name = args.browser if args.browser is not None else "chrome"
-    print(Instagram.scrap(args.username, browser_name))
+    print(Instagram.scrap(args.username, args.proxy))
 
 
-# last updated on 29th December, 2022
+# last updated on 1st January, 2022
